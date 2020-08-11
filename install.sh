f32e43ca3ea3aaac54d70011cf0c91172ff1940e
3m0W33D <sky.t4y@gmail.com>
2020-08-11 14:05:22 +0000

    Added pwncat and fixed pwntools uninstall

--- install.sh
+++
@@ -1,1 +0,0 @@
-dpkg --add-architecture i386 && apt update && apt install -y gdbserver build-essential libtool g++ gcc texinfo curl wget automake autoconf python python-dev git subversion unzip virtualenvwrapper lsb-release
